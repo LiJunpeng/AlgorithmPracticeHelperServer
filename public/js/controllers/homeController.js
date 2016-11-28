860ec297406cde83b78c5c7c5ec0861b39e88704
LiJunpeng <lijunpeng91@gmail.com>
2016-11-28 01:22:07 -0500

    use <pre> to make text break line in div

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -1,5 +1,7 @@
 var app = angular.module("AlgorithmHelper");
 
+
+
 app.controller("homeController",
     ["$scope", "$http", "$routeParams", "$location", function ($scope, $http, $routeParams, $location) {
     // $http.get("/api/v1/urls/" + $routeParams.shortUrl)
@@ -9,15 +11,25 @@ app.controller("homeController",
     //         $scope.shortUrlToShow = "http://localhost:3000/" + data.shortUrl;
     //     });
 
+    $scope.selectedRow = null;  // initialize our variable to null
+        $scope.setClickedRow = function(index){  //function that sets the value of selectedRow to current index
+        $scope.selectedRow = index;
+        console.log($scope.problems[index].name);
+
+        var url = "/api/v1/AlgoProblems/" + $scope.problems[index].name.replace(" ", "%20");
+        $http.get(url)
+            .success(function (data) {
+                $scope.problem_detail = data;
+                console.log(data);
+                prettyPrint();
+        });
+    }
 
     $http.get("/api/v1/AlgoProblems")
         .success(function (data) {
             console.log(data);
-           // var names = data.name_list.split('|');
-            //$scope.problems = names;
             $scope.problems = data;
-
-        });
+    });
 
 
     $scope.goToAddPage = function () {
